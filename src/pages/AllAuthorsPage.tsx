be100d678c7a04c9c8d1c2f51acdca74860d62ab
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-05-26 16:52:56 +0200

    Authors controls and basic list of all authors addded

--- src/pages/AllAuthorsPage.tsx
+++ src/pages/AllAuthorsPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useAppSelector } from '../store';
 
-const AllAutorsPage = () => <h1>All autors</h1>;
+const AllAuthorsPage = () => {
+	const authors = useAppSelector((state) => state.authors);
 
-export default AllAutorsPage;
+	return (
+		(
+			<div>
+				<button>Dodaj</button>
+				<table>
+					{authors?.map((author) => (
+						<tr key={author.id}>
+							<td>{author.id}</td>
+							<td> {author.firstName}</td> <td> {author.lastName}</td>
+							<td>
+								<button>Edytuj</button> <button>Usuń</button>
+							</td>
+						</tr>
+					))}
+				</table>
+			</div>
+		) ?? <p>Loading..</p>
+	);
+};
+
+export default AllAuthorsPage;
