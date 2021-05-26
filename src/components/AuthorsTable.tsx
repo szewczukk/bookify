84369ab074980051fd4d795597542181646fde0f
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-05-26 19:54:29 +0200

    AuthorsTable is now given a proper HTML structure

--- src/components/AuthorsTable.tsx
+++ src/components/AuthorsTable.tsx
@@ -8,16 +8,27 @@ const AllAuthorsPage = () => {
 
 	return (
 		<table>
-			{authors?.map((author) => (
-				<tr key={author.id} data-cy={`author-${author.lastName}`}>
-					<td>{author.id}</td>
-					<td> {author.firstName}</td> <td> {author.lastName}</td>
-					<td>
-						<button>Edytuj</button>
-						<DeleteAuthor id={author.id} />
-					</td>
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Imię</th>
+					<th>Nazwisko</th>
+					<th>Akcje</th>
 				</tr>
-			))}
+			</thead>
+			<tbody>
+				{authors?.map((author) => (
+					<tr key={author.id} data-cy={`author-${author.lastName}`}>
+						<td>{author.id}</td>
+						<td>{author.firstName}</td>
+						<td>{author.lastName}</td>
+						<td>
+							<button>Edytuj</button>
+							<DeleteAuthor id={author.id} />
+						</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	);
 };
