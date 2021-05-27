4815e5ef8462978cccb4947784608aaa639b9228
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-05-27 15:48:54 +0200

    Publishers editing modal communicate moved to the page component

--- src/pages/AllPublishersPage.tsx
+++ src/pages/AllPublishersPage.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import PublishersTable from '../components/PublishersTable';
+import { useAppDispatch, useAppSelector } from '../store';
+import { actions as modalActions } from '../store/modal';
+import { actions as publishersActions } from '../store/publishers';
 
-const AllPublishersPage = () => (
-	<main>
-		<Link to="/create-publisher" data-cy="link-create-publisher">
-			Dodaj
-		</Link>
-		<PublishersTable />
-	</main>
-);
+const AllPublishersPage = () => {
+	const { status } = useAppSelector((state) => state.publishers);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		if (['success', 'error'].includes(status)) {
+			dispatch(modalActions.setText(status === 'success' ? 'Sukces' : 'Błąd'));
+			dispatch(modalActions.toggleModal());
+			dispatch(publishersActions.setStatus('none'));
+		}
+	}, [status]);
+
+	return (
+		<main>
+			<Link to="/create-publisher" data-cy="link-create-publisher">
+				Dodaj
+			</Link>
+			<PublishersTable />
+		</main>
+	);
+};
 
 export default AllPublishersPage;
