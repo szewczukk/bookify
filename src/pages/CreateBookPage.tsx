0147f61b317d0d0fd84c41e7b55bec77c4d15a12
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-05-27 13:10:31 +0200

    Book data validation

--- src/pages/CreateBookPage.tsx
+++ src/pages/CreateBookPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, FormikErrors } from 'formik';
 import { Link, useHistory } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../store';
 import { actions } from '../store/modal';
@@ -8,6 +8,14 @@ import { createBook } from '../store/books';
 
 interface Props extends Partial<Book> {
 	edit?: boolean;
+}
+
+interface FormValues {
+	title: string;
+	isbn: string;
+	publishmentYear: number;
+	authorId: number;
+	publisherId: number;
 }
 
 const CreateBookPage = ({
@@ -38,6 +46,20 @@ const CreateBookPage = ({
 					authorId: authorId || authors[0].id,
 					publisherId: publisherId || publishers[0].id,
 				}}
+				validate={(values) => {
+					const errors: FormikErrors<FormValues> = {};
+
+					const regex = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+
+					if (!regex.test(values.isbn)) {
+						errors.isbn = 'Zły format ISBN';
+					}
+					if (values.publishmentYear > new Date().getFullYear()) {
+						errors.publishmentYear = 'Zła data';
+					}
+
+					return errors;
+				}}
 				onSubmit={async (values) => {
 					if (edit && id) {
 						//await dispatch(editAuthor({ ...values, id }));
@@ -45,75 +67,82 @@ const CreateBookPage = ({
 						dispatch(actions.setText('Sukces'));
 						dispatch(actions.toggleModal());
 					} else {
-						await dispatch(createBook(values));
+						await dispatch(
+							createBook({ ...values, isbn: values.isbn.replaceAll('-', '') }),
+						);
 						history.push('/books');
 						dispatch(actions.setText('Sukces'));
 						dispatch(actions.toggleModal());
 					}
 				}}
 			>
-				<Form data-cy="form">
-					<label htmlFor="title">Tytuł</label>
-					<Field
-						id="title"
-						name="title"
-						placeholder="Tytuł"
-						required
-						data-cy="input-title"
-					/>
+				{({ errors }) => (
+					<Form data-cy="form">
+						<label htmlFor="title">Tytuł</label>
+						<Field
+							id="title"
+							name="title"
+							placeholder="Tytuł"
+							required
+							data-cy="input-title"
+						/>
 
-					<label htmlFor="isbn">ISBN</label>
-					<Field
-						id="isbn"
-						name="isbn"
-						placeholder="ISBN"
-						required
-						data-cy="input-isbn"
-					/>
+						<label htmlFor="isbn">ISBN</label>
+						<Field
+							id="isbn"
+							name="isbn"
+							placeholder="ISBN"
+							required
+							data-cy="input-isbn"
+						/>
 
-					<label htmlFor="publishmentYear">Rok publikacji</label>
-					<Field
-						id="publishmentYear"
-						name="publishmentYear"
-						placeholder="Rok publikacji"
-						required
-						data-cy="input-year"
-					/>
+						<label htmlFor="publishmentYear">Rok publikacji</label>
+						<Field
+							id="publishmentYear"
+							name="publishmentYear"
+							placeholder="Rok publikacji"
+							required
+							data-cy="input-year"
+						/>
 
-					<label htmlFor="authorId">Autor</label>
-					<Field
-						id="authorId"
-						name="authorId"
-						required
-						data-cy="input-author"
-						as="select"
-					>
-						{authors.map((author) => (
-							<option key={author.id} value={author.id}>
-								{author.firstName} {author.lastName}
-							</option>
-						))}
-					</Field>
+						<label htmlFor="authorId">Autor</label>
+						<Field
+							id="authorId"
+							name="authorId"
+							required
+							data-cy="input-author"
+							as="select"
+						>
+							{authors.map((author) => (
+								<option key={author.id} value={author.id}>
+									{author.firstName} {author.lastName}
+								</option>
+							))}
+						</Field>
 
-					<label htmlFor="publisherId">Wydawca</label>
-					<Field
-						id="publisherId"
-						name="publisherId"
-						required
-						data-cy="input-publisher"
-						as="select"
-					>
-						{publishers.map((publisher) => (
-							<option key={publisher.id} value={publisher.id}>
-								{publisher.name}
-							</option>
-						))}
-					</Field>
+						<label htmlFor="publisherId">Wydawca</label>
+						<Field
+							id="publisherId"
+							name="publisherId"
+							required
+							data-cy="input-publisher"
+							as="select"
+						>
+							{publishers.map((publisher) => (
+								<option key={publisher.id} value={publisher.id}>
+									{publisher.name}
+								</option>
+							))}
+						</Field>
 
-					<button type="submit" data-cy="submit">
-						Zapisz
-					</button>
-				</Form>
+						<button type="submit" data-cy="submit">
+							Zapisz
+						</button>
+
+						{errors.publishmentYear && <p>{errors.publishmentYear}</p>}
+						{errors.isbn && <p>{errors.isbn}</p>}
+					</Form>
+				)}
 			</Formik>
 		</main>
 	);
