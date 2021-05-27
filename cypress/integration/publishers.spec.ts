7d44e7f5a60d728e27a261584d2f6bd0ab4005b7
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-05-27 10:57:55 +0200

    Tests for editing a publisher added

--- cypress/integration/publishers.spec.ts
+++ cypress/integration/publishers.spec.ts
@@ -2,7 +2,7 @@ describe('Testing publishers functionality', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000/');
 	});
-	it('Create a new publishers', () => {
+	it('Create a new publisher', () => {
 		cy.get('[data-cy=link-publishers]').click();
 		cy.get('[data-cy=link-create-publisher]').click();
 
@@ -15,14 +15,36 @@ describe('Testing publishers functionality', () => {
 
 		cy.url().should('contain', 'publishers');
 
-		cy.get('[data-cy=publisher-row').should('contain', 'ABC');
-		cy.get('[data-cy=publisher-row').should('contain', '2020');
+		cy.get('[data-cy=publisher-row]').should('contain', 'ABC');
+		cy.get('[data-cy=publisher-row]').should('contain', '2020');
 	});
 
-	it('Should delete a created user', () => {
+	it('Edit the new publisher', () => {
 		cy.get('[data-cy=link-publishers]').click();
 		cy.get('[data-cy=publisher-row')
 			.contains('ABC')
+			.parent()
+			.within(() => {
+				cy.get('[data-cy=edit]').click();
+			});
+
+		cy.get('[data-cy=input-name]').clear().type('Znak');
+		cy.get('[data-cy=input-year]').clear().type('1999');
+		cy.get('[data-cy=form]').submit();
+
+		cy.get('[data-cy=modal-text]').should('contain', 'Sukces');
+		cy.get('[data-cy=modal-submit]').click();
+
+		cy.url().should('contain', 'publishers');
+
+		cy.get('[data-cy=publisher-row]').should('contain', 'Znak');
+		cy.get('[data-cy=publisher-row]').should('contain', '1999');
+	});
+
+	it('Should delete a created publisher', () => {
+		cy.get('[data-cy=link-publishers]').click();
+		cy.get('[data-cy=publisher-row')
+			.contains('Znak')
 			.parent()
 			.within(() => {
 				cy.get('[data-cy=delete]')
